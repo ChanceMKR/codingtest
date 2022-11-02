dfe483071c4bcb3ee44f00a5ab368f7a912fd351
ChanceMKR <87632730+ChanceMKR@users.noreply.github.com>
2022-11-02 21:36:53 +0900

    py day1

---
+++ 1018.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const input = fs.readFileSync("./1018.txt").toString().split("\n")
+
+const arr = input[0].split(" ")
+const n = arr[0]
+const m = arr[1]
+
+console.log(input)
